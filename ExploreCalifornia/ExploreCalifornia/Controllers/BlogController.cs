4fd2f35b680f5bf5facf4795b3640346550510de
albert18 <caps18@hotmail.com>
2019-11-06 16:12:09 -0500

    update render with strongly typed view

--- ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.Controllers
 {
-    [Route("blog")]
+    //[Route("blog")]
 
     public class BlogController : Controller
     {
@@ -16,10 +17,20 @@ namespace ExploreCalifornia.Controllers
             return View();
         }
 
-        [Route("{year:int}/{month:int}/{key}")]
+        [Route("blog/{year:int}/{month:int}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            return View();
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Albert",
+                Body = "This is a great blog"
+            };
+
+
+
+            return View(post);
             //return new ContentResult { Content = string.Format("Year: {0}, Month: {1}, Key: {2}", year,month,key) };
             
         }
