3f4184c0bfdba912612a7943ec3350c6aeace9c6
albert18 <caps18@hotmail.com>
2019-11-06 16:56:46 -0500

    update

--- ExploreCalifornia/ExploreCalifornia/Models/Post.cs
+++ ExploreCalifornia/ExploreCalifornia/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,21 @@ namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Display(Name = "Post Title")]
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100")]
         public string Title { get; set; }
+
         public string Author { get; set; }
+
+
+        [Display(Name = "Message")]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(100, ErrorMessage = "Title mustat least 100 character")]
         public string Body { get; set; }
+
         public DateTime Posted { get; set; }
     }
 }
