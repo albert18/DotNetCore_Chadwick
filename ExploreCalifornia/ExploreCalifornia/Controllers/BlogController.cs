549ec1ccadf6cec2ad2245e58f0c6fa8df0dbb78
albert18 <caps18@hotmail.com>
2019-11-06 10:33:26 -0500

    update

--- ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
@@ -12,5 +12,10 @@ namespace ExploreCalifornia.Controllers
         {
             return View();
         }
+
+        public IActionResult Post(string id)
+        {
+            return new ContentResult {  Content = id };
+        }
     }
 }
