87d4f796204631417c071981ca7259783d271b64
albert18 <erertech@gmail.com>
2019-11-09 17:28:32 -0500

    update

--- ExploreCalifornia/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
+++ ExploreCalifornia/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.ViewComponents
 {
-    public class MonthlySpecialsViewComponent
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
     {
+        public string Invoke()
+        {
+            return "Resuable Components";
+        }
+            
     }
 }
